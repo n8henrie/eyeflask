227beb429cbf8d6698a285da6e11aae9bb53998b
Nathan Henrie <nate@n8henrie.com>
2017-03-10 12:15:55 -0700

    Add missing comma

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5"
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
     extras_require={
         "dev": dev_requirements
